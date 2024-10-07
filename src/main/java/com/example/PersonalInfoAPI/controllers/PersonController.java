b72b6c1a2f8c13a3ffa25cbdb8f89ded28e6c565
Thales Bensi <thalesbensi@outlook.com>
2024-10-07 19:17:34 -0300

    findAll implemented

--- src/main/java/com/example/PersonalInfoAPI/controllers/PersonController.java
+++ src/main/java/com/example/PersonalInfoAPI/controllers/PersonController.java
@@ -1,11 +1,12 @@
 package com.example.PersonalInfoAPI.controllers;
 
 import com.example.PersonalInfoAPI.services.PersonServices;
-import com.fasterxml.jackson.annotation.JsonAlias;
 import model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -16,12 +17,19 @@ public class PersonController {
     private PersonServices service;
     //private PersonServices service = new PersonServices(); <-- Before Autowired
 
+    @RequestMapping(method= RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List findAll(){
+        return service.findAll();
+    }
+
     @RequestMapping(value = "{id}",
             method= RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-
     public Person findbyId(@PathVariable(value = "id")String id) throws Exception{
-        return service.findbyid();
+        return service.findbyid(id);
     }
+
+
 
 }
--- src/main/java/com/example/PersonalInfoAPI/services/PersonServices.java
+++ src/main/java/com/example/PersonalInfoAPI/services/PersonServices.java
@@ -3,6 +3,8 @@ package com.example.PersonalInfoAPI.services;
 import model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,7 +14,7 @@ public class PersonServices {
     private final AtomicLong counter =  new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
-    public Person findbyid() {
+    public Person findbyid(String id) {
 
         logger.info("Finding a person!");
         Person person = new Person();
@@ -24,4 +26,28 @@ public class PersonServices {
 
         return person;
     }
+
+    public List findAll() {
+
+        logger.info("Finding all people!");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Thales" + i);
+        person.setLastName("Bensi" + i);
+        person.setAdress("Some adress in Brasil" + i);
+        person.setGender((i % 2 == 0) ? "Male" : "Female");;
+
+        return person;
+    }
+
 }
