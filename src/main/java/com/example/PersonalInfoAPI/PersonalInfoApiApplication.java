fc66cd18062d9ac847165eb020fde4a8cafd6fee
Thales Bensi <thalesbensi@outlook.com>
2024-10-07 15:39:47 -0300

    API Setup

---
+++ src/main/java/com/example/PersonalInfoAPI/PersonalInfoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.PersonalInfoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalInfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalInfoApiApplication.class, args);
+	}
+
+}
