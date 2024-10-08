eed0b39dc712174869149d4029502b2e6d374e8c
Thales Bensi <thalesbensi@outlook.com>
2024-10-08 18:46:23 -0300

    MySQL connection

---
+++ src/main/java/com/example/PersonalInfoAPI/Repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.example.PersonalInfoAPI.Repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
